de4badfb8d20c84a13cde65b045ca637fa89632a
Chad.Snyder <Mapletonkid@gmail.com>
2019-12-20 11:34:50 -0700

    MVP

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from './CharacterCard';
+import styled from 'styled-components';
 
-export default function SearchForm() {
+const StyledCard = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;    
+    margin: 1%;   
+`;
+
+export default function SearchForm(props) {
  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect (() => { 
+    const data = props.characters.filter(character => character.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setSearchResults(data);
+  },[searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+ 
+
   return (
     <section className="search-form">
-     // Add a search form here
-    </section>
+     <form className="search">
+       <input type="text" name="textfield" placeholder="Search" value={searchTerm} onChange={handleChange} />
+     </form>
+
+      {
+        searchResults.length === 0
+        ? (<StyledCard>
+        {props.characters.map(character => (
+          <CharacterCard key={character.id} name={character.name} img={character.image} species={character.species} status={character.status} />
+        ))}
+      </StyledCard>) 
+      : (<StyledCard>
+        {searchResults.map(character => (
+          <CharacterCard key={character.id} name={character.name} img={character.image} species={character.species} status={character.status} />
+        ))}
+      </StyledCard>)
+      }
+
+  </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,28 @@
 import React from "react";
+import '../../src/MainImg.css'
+import styled from 'styled-components';
+
+const Img = styled.img`
+  width: 100vh;
+  border-radius: 8px;
+  box-shadow: 2px 2px 0 0 black;
+`;
+
+const Quote = styled.h3`
+  text-decoration: wavy;
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        <Quote>Yeah, welcome to the club, pal.</Quote>
       </header>
     </section>
   );
